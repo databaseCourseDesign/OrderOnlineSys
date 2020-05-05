89245524e93cd6b18a5acf7768c8f1b76b7bec43
xo-planet <wanghj_s@tju.edu.cn>
2020-05-05 23:57:46 +0800

    ‘调整页面，实现登录

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-#-*- coding=utf-8 -*-
+# -*- coding=utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import MySQLdb
 import os
 import sys
 import importlib
+
 importlib.reload(sys)
 
 app = Flask(__name__)
@@ -14,24 +15,122 @@ username = "TJU"
 userRole = "CUSTOMER"
 notFinishedNum = 0
 
+
 @app.route('/')
 @app.route('/index')
-# 首页进行登录，可选择进入注册
+# 首页
 def indexpage():
-	return render_template('index.html')
+    return render_template('index.html')
+
+
+# 注册
+@app.route('/register', methods=['GET', 'POST'])
+def registerPage():
+    return render_template('Register.html')
+
+
+# 登录
+@app.route('/logIn', methods=['GET', 'POST'])
+def logInPage():
+    global username
+    global userRole
+    msg = ""
+    if request.method == 'GET':
+        return render_template('logIn.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        userRole = request.form.get('userRole')
+        print(userRole)
+        print(username)
+        # 连接数据库，默认数据库用户名root，密码空
+        db = MySQLdb.connect("localhost", "root", "whj123", "appDB", charset='utf8')
+
+        if userRole == 'ADMIN':
+            cursor = db.cursor()
+            try:
+                cursor.execute("use appDB")
+            except:
+                print("Error: unable to use database!")
+            sql = "SELECT * from ADMIN where username = '{}' and password='{}'".format(username, password)
+            cursor.execute(sql)
+            db.commit()
+            res = cursor.fetchall()
+            num = 0
+            for row in res:
+                num = num + 1
+            # 如果存在该管理员且密码正确
+            if num == 1:
+                print("登录成功！欢迎管理员！")
+                msg = "done1"
+            else:
+                print("您没有管理员权限或登录信息出错。")
+                msg = "fail1"
+            return render_template('logIn.html', messages=msg, username=username, userRole=userRole)
+
+        elif userRole == 'RESTAURANT':
+            cursor = db.cursor()
+            try:
+                cursor.execute("use appDB")
+            except:
+                print("Error: unable to use database!")
+            sql = "SELECT * from RESTAURANT where username = '{}' and password='{}'".format(username, password)
+            cursor.execute(sql)
+            db.commit()
+            res = cursor.fetchall()
+            num = 0
+            for row in res:
+                num = num + 1
+            # 如果存在该商家且密码正确
+            if num == 1:
+                print("登录成功！欢迎商家用户！")
+                msg = "done2"
+            else:
+                print("您没有商家用户权限或登录信息出错。")
+                msg = "fail2"
+            return render_template('logIn.html', messages=msg, username=username, userRole=userRole)
+
+        elif userRole == 'CUSTOMER':
+            cursor = db.cursor()
+            try:
+                cursor.execute("use appDB")
+            except:
+                print("Error: unable to use database!")
+            sql = "SELECT * from CUSTOMER where username = '{}' and password='{}'".format(username, password)
+            cursor.execute(sql)
+            db.commit()
+            res = cursor.fetchall()
+            num = 0
+            for row in res:
+                num = num + 1
+            # 如果存在该用户且密码正确
+            if num == 1:
+                print("登录成功！欢迎用户！")
+                msg = "done3"
+            else:
+                print("您没有用户权限，未注册或登录信息出错。")
+                msg = "fail3"
+            return render_template('logIn.html', messages=msg, username=username, userRole=userRole)
+
+
+# 管理员的店铺列表页面
+@app.route('/adminRestList')
+def adminRestListPage():
+    return render_template('adminRestList.html')
 
 # 个人中心页面
 @app.route('/personal')
 def personalPage():
     return render_template('personal.html')
 
+
 # 修改个人信息页面
 @app.route('/ModifyPersonalInfo', methods=['GET', 'POST'])
 def ModifyPersonalInfo():
-    msg=""
+    msg = ""
     if request.method == 'GET':
         return render_template('ModifyPersonalInfo.html', username=username)
-    if request.method =='POST':
+    if request.method == 'POST':
         # username = request.form['username']
         address = request.form['address']
         phonenum = request.form['phonenum']
@@ -42,25 +141,27 @@ def ModifyPersonalInfo():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        sql = "Update {} SET address = '{}', phone = '{}' where username = '{}'".format(userRole, address, phonenum, username)
+        sql = "Update {} SET address = '{}', phone = '{}' where username = '{}'".format(userRole, address, phonenum,
+                                                                                        username)
         try:
             cursor.execute(sql)
             db.commit()
             # print("修改个人信息成功")
-            msg="done"
+            msg = "done"
         except ValueError as e:
             print("--->", e)
             print("修改个人信息失败")
-            msg="fail"
+            msg = "fail"
         return render_template('ModifyPersonalInfo.html', messages=msg, username=username)
+
 
 # 修改密码页面
 @app.route('/ModifyPassword', methods=['GET', 'POST'])
 def ModifyPassword():
-    msg=""
+    msg = ""
     if request.method == 'GET':
         return render_template('ModifyPassword.html', username=username)
-    if request.method =='POST':
+    if request.method == 'POST':
         # username = request.form['username']
         psw1 = request.form['psw1']
         psw2 = request.form['psw2']
@@ -78,15 +179,16 @@ def ModifyPassword():
                 cursor.execute(sql)
                 db.commit()
                 # print("修改密码成功")
-                msg="done"
+                msg = "done"
             except ValueError as e:
                 print("--->", e)
                 print("修改密码失败")
-                msg="fail"
+                msg = "fail"
             return render_template('ModifyPassword.html', messages=msg, username=username)
         else:
-            msg="not equal"
+            msg = "not equal"
             return render_template('ModifyPassword.html', messages=msg, username=username)
+
 
 @app.route('/OrderPage', methods=['GET', 'POST'])
 def OrderPage():
@@ -115,7 +217,8 @@ def OrderPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('OrderPage.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('OrderPage.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -127,7 +230,7 @@ def OrderPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' Order BY tansactiontime DESC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -136,7 +239,8 @@ def OrderPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('OrderPage.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('OrderPage.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -148,7 +252,7 @@ def OrderPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' Order BY cost ASC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -157,7 +261,8 @@ def OrderPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('OrderPage.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('OrderPage.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -169,7 +274,7 @@ def OrderPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' AND isFinished = 0 " % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -178,7 +283,8 @@ def OrderPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('OrderPage.html', username=username, result = res, messages=msg, notFinishedNum=len(res))
+            return render_template('OrderPage.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=len(res))
         else:
             print("NULL")
             msg = "none"
@@ -196,9 +302,10 @@ def OrderPage():
         sql = "Update ORDER_COMMENT SET isFinished = 1 WHERE orderID = '%s' " % orderID
         cursor.execute(sql)
         msg = "UpdateSucceed"
-        return render_template('OrderPage.html', username=username, messages=msg)    
+        return render_template('OrderPage.html', username=username, messages=msg)
     else:
         return render_template('OrderPage.html', username=username, messages=msg)
+
 
 @app.route('/MyComments', methods=['GET', 'POST'])
 def MyCommentsPage():
@@ -227,7 +334,8 @@ def MyCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('MyComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('MyComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -239,7 +347,7 @@ def MyCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' AND isFinished = 1 AND text is not null Order BY tansactiontime DESC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -248,7 +356,8 @@ def MyCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('MyComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('MyComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -260,7 +369,7 @@ def MyCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' AND isFinished = 1 AND text is not null Order BY cost ASC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -269,7 +378,8 @@ def MyCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('MyComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('MyComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -282,7 +392,7 @@ def MyCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' AND isFinished = 1 AND text is null" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -291,7 +401,8 @@ def MyCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('MyComments.html', username=username, result = res, messages=msg, notFinishedNum=len(res))
+            return render_template('MyComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=len(res))
         else:
             print("NULL")
             msg = "none"
@@ -299,6 +410,7 @@ def MyCommentsPage():
 
     else:
         return render_template('MyComments.html', username=username, messages=msg)
+
 
 @app.route('/WriteComments', methods=['GET', 'POST'])
 def WriteCommentsPage():
@@ -325,7 +437,8 @@ def WriteCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('WriteComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('WriteComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -337,7 +450,7 @@ def WriteCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' Order BY tansactiontime DESC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -346,7 +459,8 @@ def WriteCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('WriteComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('WriteComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -358,7 +472,7 @@ def WriteCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' Order BY cost ASC" % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -367,7 +481,8 @@ def WriteCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('WriteComments.html', username=username, result = res, messages=msg, notFinishedNum=notFinishedNum)
+            return render_template('WriteComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=notFinishedNum)
         else:
             print("NULL")
             msg = "none"
@@ -379,7 +494,7 @@ def WriteCommentsPage():
             cursor.execute("use appDB")
         except:
             print("Error: unable to use database!")
-        
+
         sql = "SELECT * FROM ORDER_COMMENT WHERE username = '%s' AND isFinished = 0 " % username
         cursor.execute(sql)
         res = cursor.fetchall()
@@ -388,7 +503,8 @@ def WriteCommentsPage():
         if len(res):
             msg = "done"
             print(msg)
-            return render_template('WriteComments.html', username=username, result = res, messages=msg, notFinishedNum=len(res))
+            return render_template('WriteComments.html', username=username, result=res, messages=msg,
+                                   notFinishedNum=len(res))
         else:
             print("NULL")
             msg = "none"
@@ -406,6 +522,7 @@ def WriteCommentsPage():
     else:
         return render_template('WriteComments.html', username=username, messages=msg)
 
+
 @app.route('/CommentForm', methods=['GET', 'POST'])
 def CommentFormPage():
     msg = ""
@@ -416,6 +533,7 @@ def CommentFormPage():
         msg = "WriteRequest"
         print(msg)
         return render_template('CommentForm.html', username=username, orderID=orderID, messages=msg)
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port='9090')
